fef497422f133dce473ce905583a4fbd1d5a4ea8
adityab <aditya@adityabhatt.org>
2012-03-21 09:58:13 +0530

    use express and handle POST

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+var sys = require('sys');
+var querystring = require('querystring');
+var fs = require('fs');
+var express = require('express');
 var summarizer = require('./lib/handler.js');
 
-summarizer.summarize("KONY", function(summary) {
-    console.log(summary);
+var app = express.createServer();
+
+app.use(express.bodyParser());
+app.use(app.router);
+
+app.get('/', function(req, res) {
+    fs.readFile('index.html', function(err, page) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(page);
+        res.end();
+    });
 });
+
+app.post('/', function(req, res) {
+    summarizer.summarize(req.body.query, function(summary) {
+        res.end(summary);
+    });
+});
+
+app.listen(8080);
